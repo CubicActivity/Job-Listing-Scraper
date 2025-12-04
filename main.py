35853cf284c4a30b5624292d4dc74fb564f0137c
Angel Stojmanovski <107684060+CubicActivity@users.noreply.github.com>
2025-12-04 05:13:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ if __name__ == "__main__":
         fetch_remoteok,
     ]
     
-    # single thread benchmark
+    # single processes benchmark
     t0 = time.time()
     serial_jobs = []
-    for f in tqdm(fetchers, desc="Serial fetching"):
+    for f in tqdm(fetchers, desc="Single process fetching"):
         serial_jobs.extend(f())
     serial_time = time.time() - t0
-    print(f"Single thread collected {len(serial_jobs)} jobs in {serial_time:.2f}s")
+    print(f"Single processes collected {len(serial_jobs)} jobs in {serial_time:.2f}s")
 
-    # 4 threads benchmark 
+    # 4 processes benchmark 
     t1 = time.time()
     parallel_jobs = collect_jobs_parallel(fetchers, processes=4)
     parallel_time = time.time() - t1
-    print(f"Parallel (4 threads) collected {len(parallel_jobs)} jobs in {parallel_time:.2f}s")
+    print(f"Parallel (4 processes) collected {len(parallel_jobs)} jobs in {parallel_time:.2f}s")
 
-    # 8 threads benchmark 
+    # 8 processes benchmark 
     t2 = time.time()
     parallel_jobs = collect_jobs_parallel(fetchers, processes=8)
     parallel_time = time.time() - t2
-    print(f"Parallel (8 threads) collected {len(parallel_jobs)} jobs in {parallel_time:.2f}s")
+    print(f"Parallel (8 processes) collected {len(parallel_jobs)} jobs in {parallel_time:.2f}s")
 
     save_jobs(parallel_jobs)
     print(f"\nSpeedup factor in Parallel: {serial_time / parallel_time:.2f}x")
