ce50435beeaf1370c5132bd6039ea25ce97a8fae
CubicActivity <stojmanovski355@gmail.com>
2026-02-08 23:52:44 +0100

    commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 from utils import *
 from Fetchers.remotive import fetch_remotive
@@ -25,14 +26,14 @@ if __name__ == "__main__":
     t1 = time.time()
     parallel_jobs = fetch_jobs(fetchers, processes=4)
     parallel_time = time.time() - t1
-    print(f"Parallel (4 processes) collected {len(parallel_jobs)} jobs in {parallel_time:.2f}s")
+    print(f"Parallel (4 process) collected {len(parallel_jobs)} jobs in {parallel_time:.2f}s")
 
     # 8 processes benchmark
     t2 = time.time()
     parallel_jobs2 = fetch_jobs(fetchers, processes=8)
     parallel_time2 = time.time() - t2
-    print(f"Parallel (8 processes) collected {len(parallel_jobs2)} jobs in {parallel_time2:.2f}s")
+    print(f"Parallel (8 process) collected {len(parallel_jobs2)} jobs in {parallel_time2:.2f}s")
 
-    export_jobs(parallel_jobs)
-    print(f"Speedup factor in Parallel: {serial_time / min(parallel_time2,parallel_time):.2f}x")
+    export_jobs(parallel_jobs, str(datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S").replace(":", "-")))
+    print(f"Best speedup factor in parallel: {serial_time / min(parallel_time2,parallel_time):.2f}x")
 
